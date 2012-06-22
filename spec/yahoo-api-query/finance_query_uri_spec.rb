aa0d9a90d3a0f2c6e30bae523219f54b02e69f02
James Whinfrey <james@conceptric.co.uk>
2012-06-22 12:52:19 +0100

    Extracts a shared example for the Finance URI specs

--- spec/yahoo-api-query/finance_query_uri_spec.rb
+++ spec/yahoo-api-query/finance_query_uri_spec.rb
@@ -1,36 +1,32 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe YahooApiQuery::Finance::QueryURI do 
-  
-  context "with a single ticker provided" do
-    subject { YahooApiQuery::Finance::QueryURI.build(["BP.L"]) }
-
+  shared_examples "valid finance URI" do |valid_ticker_string|
     it "returns a URI object for the Yahoo Finance Query" do
       subject.should be_instance_of URI::HTTP 
     end
 
-    it "should be a URL to the Yahoo API" do
+    it "should be a Yahoo! API URL" do
       subject.scheme.should == "http"    
       subject.host.should == "query.yahooapis.com"
       subject.path.should == "/v1/public/yql"
     end
 
-    it "should contain the correct query string" do
-      subject.query.should == "q=select%20symbol,%20Ask,%20Bid%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22BP.L%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
-    end
-
     it "should parse the correct URL string" do
-      expected = "http://query.yahooapis.com/v1/public/yql?q=select%20symbol,%20Ask,%20Bid%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22BP.L%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+      expected = "http://query.yahooapis.com/v1/public/yql?q=select%20symbol,%20Ask,%20Bid%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(#{valid_ticker_string})&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
       subject.to_s.should == expected
-    end    
+    end        
+  end
+
+  context "with a single ticker provided" do
+    subject { YahooApiQuery::Finance::QueryURI.build(["BP.L"]) }   
+    include_examples "valid finance URI", '%22BP.L%22'
   end  
   
   context "with three tickers provided" do
     subject { YahooApiQuery::Finance::QueryURI.build(["BP.L","BLT.L","GSK.L"]) }
-
-    it "should contain the correct query string" do
-      subject.query.should == "q=select%20symbol,%20Ask,%20Bid%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22BP.L%22,%20%22BLT.L%22,%20%22GSK.L%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
-    end    
+    include_examples "valid finance URI", 
+      '%22BP.L%22,%20%22BLT.L%22,%20%22GSK.L%22'
   end                                 
   
   context "without any tickers provided" do
