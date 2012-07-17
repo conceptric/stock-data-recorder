904e4de06e3234d52d8939992cf6587e9413f75e
James Whinfrey <james@conceptric.co.uk>
2012-07-17 13:19:09 +0100

    Cleans up the specs a little

--- spec/stock-data-recorder/quoted_item_spec.rb
+++ spec/stock-data-recorder/quoted_item_spec.rb
@@ -1,56 +1,57 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe Stock::Data::QuotedItem do
-  context "when created with valid attributes" do
+describe Stock::Data::QuotedItem do 
+  
+  describe ".new instance of QuotedItem" do
     
-    subject do
-      Stock::Data::QuotedItem.new("BP.L")
+    context "when created with valid attributes" do
+    
+      subject do
+        Stock::Data::QuotedItem.new("BP.L")
+      end
+  
+      it "creates an instance" do      
+        subject.should be_instance_of Stock::Data::QuotedItem
+      end                    
+
+      it "has a ticker" do
+        subject.ticker.should == "BP.L"
+      end                          
+  
     end
   
-    it "creates an instance" do      
-      subject.should be_instance_of Stock::Data::QuotedItem
-    end                    
-
-    it "has a ticker" do
-      subject.ticker.should == "BP.L"
-    end                          
-  
-  end
-  
-  context "when created with invalid attributes" do
-    
-    describe "raises an ArgumentError with invalid ticker types" do
-      
-      it "like a missing argument" do
+    context "when created with invalid attributes" do
+         
+      it "raises an error with a missing argument" do
         expect {Stock::Data::QuotedItem.new()}.to raise_error ArgumentError
       end
-      
-      it "like nil" do      
+    
+      it "raises an error with nil" do      
         invalid_ticker_helper(nil, "Ticker must be a valid string")
       end                    
 
-      it "like an integer" do      
+      it "raises an error with an integer" do      
         invalid_ticker_helper(10, "Ticker must be a valid string")
       end                          
 
-      it "like a boolean" do      
+      it "raises an error with a boolean" do      
         invalid_ticker_helper(true, "Ticker must be a valid string")
       end                          
-    
-      it "like a blank string" do      
+  
+      it "raises an error with a blank string" do      
         invalid_ticker_helper("", "You must provide a ticker")
       end                        
 
-      it "like a market string without a ticker" do      
+      it "raises an error with a market id without a ticker" do      
         invalid_ticker_helper(".L", "You must provide a ticker")
       end                        
-    
-      it "like a ticker without a market string" do      
+  
+      it "raises an error with a ticker without a market id" do      
         invalid_ticker_helper("BP", "You must provide a market ID")
       end                        
 
-    end    
-
+    end
+   
   end
-
+  
 end
