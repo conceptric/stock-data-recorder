403b09f467f9b5ed63c374f93baa53a83e137660
James Whinfrey <james@conceptric.co.uk>
2012-07-20 12:10:10 +0100

    Adds a new collection class for price lists

---
+++ spec/stock-data-recorder/price_list_spec.rb
@@ -0,0 +1,52 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+module Stock
+  module Data
+    class PriceList
+
+      include Enumerable
+      extend Forwardable
+      def_delegators :@prices, :empty?, :size, :each
+      
+      def initialize()
+        @prices = []
+      end           
+      
+      def <<(price)     
+        @prices << price if price.class == Stock::Data::QuotedPrice
+      end
+      
+    end
+  end
+end
+
+describe Stock::Data::PriceList do
+  
+  subject { Stock::Data::PriceList.new }
+  
+  it { should be_empty }
+
+  it "allows a quoted price to be added" do  
+    subject << Stock::Data::QuotedPrice.new(
+      DateTime.new(2012,06,01), 2, 1)
+    subject.size.should == 1
+  end                                             
+  
+  it "will only add a Quoted Price" do
+    subject << 1
+    subject << 1.0
+    subject << '1'
+    subject << true
+    subject << [1]
+    subject << {one:1}
+    subject.should be_empty
+  end  
+  
+  it "allows iteration through the contents" do
+    subject << Stock::Data::QuotedPrice.new(
+      DateTime.new(2012,06,01), 2, 1)
+    subject.each {|item| item.should 
+      be_instance_of Stock::Data::QuotedPrice }
+  end
+  
+end
