313dcc022fcc109dcff8db9d146dec2a72622dcb
James Whinfrey <james@conceptric.co.uk>
2012-07-24 16:23:55 +0100

    Extends the functionality to deletion, and refactors the specs

--- lib/stock-data-recorder/price_list.rb
+++ lib/stock-data-recorder/price_list.rb
@@ -4,7 +4,7 @@ module Stock
 
       include Enumerable
       extend Forwardable
-      def_delegators :@prices, :empty?, :size, :each
+      def_delegators :@prices, :empty?, :size, :each, :clear
       
       def initialize()
         @prices = []
@@ -13,7 +13,11 @@ module Stock
       def <<(price_data)                     
         @prices << QuotedPrice.new(price_data)
       end
-            
+
+      def delete_for_date(the_date)
+        @prices.delete_if { |item| item.date == the_date }
+      end
+      
     end
   end
 end
--- spec/stock-data-recorder/price_list_spec.rb
+++ spec/stock-data-recorder/price_list_spec.rb
@@ -3,29 +3,80 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Stock::Data::PriceList do
   
   subject { Stock::Data::PriceList.new }
-  let(:past)   { {date:DateTime.new(2012,01,01), ask:2, bid:1} } 
-  let(:recent) { {date:DateTime.new(2012,06,01), ask:3, bid:2.5} }
+  
+  let(:past_date)   { DateTime.new(2012,01,01) }
+  let(:recent_date) { DateTime.new(2012,06,01) }
+  let(:past)   { {date:past_date, ask:2, bid:1} } 
+  let(:recent) { {date:recent_date, ask:3, bid:2.5} }
     
   it { should be_empty }
 
-  it "allows a quoted price to be added from a suitable hash" do  
-    subject << past
-    subject.size.should == 1
-  end                                             
+  describe "<<" do
     
-  it "supports iteration through the contents" do
-    subject << past
-    subject.each {|item| item.should 
-      be_instance_of Stock::Data::QuotedPrice }
+    it "adds a quoted price based on a suitable hash" do  
+      subject << past
+      subject.size.should == 1
+    end                                             
+
+    it "raises an Argument error if supplied hash is not suitable" do
+      expect { subject << {} }.to raise_error ArgumentError
+    end  
+
+  end
+    
+  describe ".each" do
+
+    it "iterates through the contents" do
+      subject << past
+      subject.each {|item| item.should 
+        be_instance_of Stock::Data::QuotedPrice }
+    end
+    
   end
 
-  it "can be sorted from oldest to most recent" do
-    subject << recent
-    subject << past
-    subject.sort.first.date.should == past[:date]
+  describe ".sort" do
+
+    it "from oldest to most recent" do
+      subject << recent
+      subject << past
+      subject.sort.first.date.should == past[:date]
+    end
+    
   end
   
-  it "raises an Argument error if invalid price data are provided" do
-    expect { subject << {} }.to raise_error ArgumentError
-  end  
+  describe ".clear" do
+
+    it "all prices in the list" do
+      subject << past
+      subject.clear  
+      subject.size.should == 0
+    end
+    
+  end
+
+  describe ".delete_for_date" do
+    
+    it "removes a price based on a specified datetime" do
+      subject << past
+      subject.delete_for_date(past_date)  
+      subject.size.should == 0
+    end
+
+    it "removes all prices with the specified datetime" do
+      subject << past
+      subject << past
+      subject.size.should == 2      
+      subject.delete_for_date(past_date)  
+      subject.size.should == 0      
+    end
+    
+    it "only removes prices with with the specified datetime" do
+      subject << past
+      subject << recent
+      subject.delete_for_date(past_date)  
+      subject.size.should == 1      
+    end
+    
+  end
+
 end
