176bca9e003639d5bc645f6380f79eaece3de68f
James Whinfrey <james@conceptric.co.uk>
2012-08-09 11:39:19 +0100

    Simplifies the stock data recorder specs. The whole library needs rescoping, it is not aiming in the right direction yet

--- spec/stock-data-recorder/stock_data_recorder_spec.rb
+++ spec/stock-data-recorder/stock_data_recorder_spec.rb
@@ -8,42 +8,18 @@ describe Stock::Data::Recorder do
     
     subject { Stock::Data::Recorder.get(tickers) }
     
-    it "requires an array of ticker strings" do
-      expect { subject }.not_to raise_error
-    end                                                               
-    
-    it "returns an array with an entry for each ticker" do
-      subject.should be_instance_of Array
-      subject.size.should == 2
+    it "returns a collection with an entry for each ticker" do
+      subject.size.should == tickers.size
     end                 
-
-    it "each entry is a instance of quoted item" do
-      subject.each do |quoted_item|
-        quoted_item.should be_instance_of Stock::Data::QuotedItem
-      end
-    end
     
-    it "each quoted item corresponds to a ticker" do
+    it "there is one entry for each ticker" do
       quoted_tickers = []
-      subject.each do |quoted_item|
-        quoted_tickers << quoted_item.ticker
+      subject.each do |entry|
+        quoted_tickers << entry.ticker
       end                        
       quoted_tickers.should == tickers
-    end
-
-    it "each quoted item contains a single price" do
-    end
-
-    it "each price is an instance of quoted price" do      
     end
   
   end                                                
   
-  describe ".save method" do
-
-    it "serialised the Quotes to a text file as comman delimited data" do      
-    end
-
-  end
-
 end
