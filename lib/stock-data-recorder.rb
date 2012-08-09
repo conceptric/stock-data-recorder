e1049986d69be4959d8dd5383722c25cc1d964a4
James Whinfrey <james@conceptric.co.uk>
2012-08-09 12:43:54 +0100

    Adds a class variable for the tickers since they are used by almost everything else. Adds a method to write data which prompted the extraction of the tickers from the get method

--- lib/stock-data-recorder.rb
+++ lib/stock-data-recorder.rb
@@ -6,12 +6,22 @@ require "stock-data-recorder/price_list"
 module Stock
   module Data
     module Recorder
-      def self.get(tickers)  
+      @@tickers = []
+                      
+      def self.set_tickers(tickers)
+        @@tickers = tickers
+      end
+      
+      def self.get
         quoted_items = []
-        tickers.each do |ticker| 
+        @@tickers.each do |ticker| 
           quoted_items << Stock::Data::QuotedItem.new(ticker)
         end                
         quoted_items
+      end    
+      
+      def self.write_to(io_string)
+        io_string << @@tickers.join(',')
       end
     end
   end
--- spec/stock-data-recorder/stock_data_recorder_spec.rb
+++ spec/stock-data-recorder/stock_data_recorder_spec.rb
@@ -2,24 +2,59 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Stock::Data::Recorder do
 
-  let(:tickers) { %w(BP.L GSK.L) }
+  let(:tickers) { %w(BP.L GSK.L) }             
 
-  describe ".get method" do                            
+  after(:each) do
+    Stock::Data::Recorder.set_tickers([])        
+  end
     
-    subject { Stock::Data::Recorder.get(tickers) }
+  describe ".get" do                            
     
-    it "returns a collection with an entry for each ticker" do
-      subject.size.should == tickers.size
-    end                 
+    subject { Stock::Data::Recorder.get }
     
-    it "there is one entry for each ticker" do
-      quoted_tickers = []
-      subject.each do |entry|
-        quoted_tickers << entry.ticker
-      end                        
-      quoted_tickers.should == tickers
+    context "with no tickers defined" do
+      it "returns an empty collection" do
+        subject.should be_empty
+      end
+    end                             
+    
+    context "with tickers defined" do        
+      before(:each) do
+        Stock::Data::Recorder.set_tickers(tickers)        
+      end
+
+      it "returns a collection with an entry for each ticker" do
+        subject.size.should eql tickers.size
+      end                 
+
+      it "the first entry for the first ticker" do
+        subject.first.ticker.should eql tickers.first
+      end      
+
+      it "the last entry for the last ticker" do
+        subject.last.ticker.should eql tickers.last
+      end      
     end
   
   end                                                
+
+  describe ".write_to" do
+    
+    subject { Stock::Data::Recorder.write_to(StringIO.new) }
+    
+    context "with no tickers defined" do    
+      it "returns an empty StringIO" do
+        subject.string.should eql ""
+      end                                             
+    end
+
+    context "with tickers defined" do
+      it "returns a StringIO containing the tickers" do
+        Stock::Data::Recorder.set_tickers(tickers)
+        subject.string.should eql tickers.join(',')
+      end    
+    end
+    
+  end
   
 end
