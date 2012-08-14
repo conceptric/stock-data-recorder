908adf462167e986f57bc179cf4c4e941e984b0b
James Whinfrey <james@conceptric.co.uk>
2012-08-14 15:37:16 +0100

    Refactors specs to improve readability and remove repetition

--- spec/stock-data-recorder/stock_data_recorder_spec.rb
+++ spec/stock-data-recorder/stock_data_recorder_spec.rb
@@ -7,43 +7,46 @@ describe Stock::Data::Recorder do
   use_vcr_cassette "yahoo-api-query", :record => :new_episodes  
 
   describe ".new" do
-    it "returns a new instance with an array of ticker strings" do        
-      Stock::Data::Recorder.new(tickers).should be_true
+    it "with an array of ticker symbols returns a new instance" do        
+      Stock::Data::Recorder.new(tickers).should 
+        be_instance_of Stock::Data::Recorder
     end    
 
-    it "returns a new instance with a blank array" do        
-      Stock::Data::Recorder.new([]).should be_true
+    it "with an empty array returns a new instance" do        
+      Stock::Data::Recorder.new([]).should 
+        be_instance_of Stock::Data::Recorder
     end    
 
-    it "returns ArgumentError without an argument" do        
-      expect { Stock::Data::Recorder.new() }.to raise_error ArgumentError
+    it "without any arguments raises an Error" do        
+      expect { Stock::Data::Recorder.new() }.to 
+       raise_error ArgumentError
     end    
   end
   
   describe ".get" do
 
+    context "with tickers defined" do        
+      subject { Stock::Data::Recorder.new(tickers).get }     
+
+      it "returns a collection with an entry for each ticker" do
+        subject.size.should eql tickers.size
+      end                 
+
+      it "the first entry for the first ticker" do
+        subject.first['symbol'].should eql tickers.first
+      end      
+
+      it "the last entry for the last ticker" do
+        subject.last['symbol'].should eql tickers.last
+      end      
+    end
+    
     context "with no tickers defined" do
       it "returns an empty collection" do        
         Stock::Data::Recorder.new([]).get.should be_empty
       end
     end                             
-    
-    context "with tickers defined" do        
-      subject { Stock::Data::Recorder.new(tickers)}     
-
-      it "returns a collection with an entry for each ticker" do
-        subject.get.size.should eql tickers.size
-      end                 
-
-      it "the first entry for the first ticker" do
-        subject.get.first['symbol'].should eql tickers.first
-      end      
-
-      it "the last entry for the last ticker" do
-        subject.get.last['symbol'].should eql tickers.last
-      end      
-    end
-  
+      
   end                                    
   
   describe ".write_to_csv" do
@@ -55,17 +58,7 @@ describe Stock::Data::Recorder do
       output.should_receive(:close).once
     end         
     
-    context "with no tickers defined" do
-      it "returns an writes nothing" do
-        recorder = Stock::Data::Recorder.new([])
-        recorder.write_to_csv
-        output.string.should eql ""
-      end                                                   
-    end
-    
     context "with tickers defined" do
-      subject { Stock::Data::Recorder.new(tickers) }
-
       let(:symbol_regex) { '([A-Za-z]+\.[A-Za-z]+)' }
       let(:price_regex) { '([0-9]+\.[0-9]{2})' }
       let(:date_regex) { '(\d{4}(-{1}\d{2}){2})' }
@@ -77,32 +70,38 @@ describe Stock::Data::Recorder do
         date_regex + time_regex
       }
 
+      before(:each) do
+        Stock::Data::Recorder.new(tickers).write_to_csv
+      end
+
       it "writes output with a symbol value" do
-        subject.write_to_csv
         output.string.should =~ Regexp.new(symbol_regex)
       end                                                   
 
       it "writes output with a price value" do
-        subject.write_to_csv
         output.string.should =~ Regexp.new(price_regex)
       end                                                   
 
       it "writes output with a datetime value" do
-        subject.write_to_csv
         output.string.should =~ Regexp.new(date_regex)
         output.string.should =~ Regexp.new(time_regex)
       end        
       
       it "writes output as comma-delimited" do        
-        subject.write_to_csv
         output.string.should =~ Regexp.new(output_regex)
       end                                           
 
       it "writes output with a single line for each ticker" do
-        subject.write_to_csv
         output.rewind
         output.readlines.size.should eql tickers.size
       end                                                 
+    end
+    
+    context "with no tickers defined" do
+      it "returns an writes nothing" do
+        Stock::Data::Recorder.new([]).write_to_csv
+        output.string.should eql ""
+      end                                                   
     end
     
   end            
